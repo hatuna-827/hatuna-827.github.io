67dfcb0217554765bb8a73318f201d09f1452b8c
hatuna-827 <hatuna827yadaka@gmail.com>
2024-11-29 22:51:18 +0900

    fix import css js

--- hatuna-827.js
+++ hatuna-827.js
@@ -1,8 +1,1 @@
 document.getElementById('homebar').innerHTML="<a href='https://hatuna-827.github.io/'>hatuna-827</a>";
-
-function copy(target_id){
-    var target = document.getElementById("code"+target_id);
-    navigator.clipboard.writeText(target.textContent);
-    let text = document.getElementById("button"+target_id).textContent;
-    document.getElementById("button"+target_id).textContent = 'copied!';
-}
