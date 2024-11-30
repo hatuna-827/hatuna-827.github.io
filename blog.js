14183210a2be6af94acc1752c429d6c5dbff3ab8
hatuna-827 <hatuna827yadaka@gmail.com>
2024-11-30 22:56:01 +0900

    add code_css & pre_js

--- blog.js
+++ blog.js
@@ -1,6 +1,12 @@
+let codecount=document.getElementsByTagName('pre').length;
+for(let i=1 ; i<=codecount ; i++){
+    document.getElementById('code'+i).insertAdjacentHTML('beforebegin','<div id="button"><button id="button'+i+'" onclick="copy('+i+')">copy</button></div>');
+}
 function copy(target_id){
     var target = document.getElementById("code"+target_id);
     navigator.clipboard.writeText(target.textContent);
-    let text = document.getElementById("button"+target_id).textContent;
     document.getElementById("button"+target_id).textContent = 'copied!';
+    setTimeout(function() {
+        document.getElementById("button"+target_id).textContent = 'copy';
+    }, 1000);
 }
