39645599c7f64edc5f852b5bc11002894131d799
hatuna-827 <hatuna827yadaka@gmail.com>
2024-12-01 22:34:12 +0900

    add js_img_alt

--- blog.js
+++ blog.js
@@ -2,6 +2,10 @@ let codecount=document.getElementsByTagName('pre').length;
 for(let i=1 ; i<=codecount ; i++){
     document.getElementById('code'+i).insertAdjacentHTML('beforebegin','<div id="button"><button id="button'+i+'" onclick="copy('+i+')">copy</button></div>');
 }
+var img = document.getElementsByTagName('img');
+for(let i=0 ; i<img.length ; i++){
+    img[i].setAttribute("alt","画像の読み込みに失敗しました。");
+}
 function copy(target_id){
     var target = document.getElementById("code"+target_id);
     navigator.clipboard.writeText(target.textContent);
