05570cd03bbf80fdbc4525d586a4f214eee34daa
hatuna-827 <hatuna827yadaka@gmail.com>
2024-12-10 16:20:54 +0900

    fix event

--- hatuna-827.js
+++ hatuna-827.js
@@ -1,2 +1,2 @@
-document.getElementById('homebar').innerHTML="<a href='https://hatuna-827.github.io/link'>❄ hatuna-827 </a><a href='https://hatuna-827.github.io/event'>❄</a>";
+document.getElementById('homebar').innerHTML="<a href='https://hatuna-827.github.io/link'>❄ hatuna-827 ❄</a>";
 /* document.getElementById('homebar').innerHTML="<a href='https://hatuna-827.github.io/link'>hatuna-827</a>"; */
