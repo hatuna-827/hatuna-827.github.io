49818dda674c9113ebf662a6585e46cdd1574961
hatuna-827 <hatuna827yadaka@gmail.com>
2025-01-31 16:58:20 +0900

    add auto js

--- blog/blog.js
+++ blog/blog.js
@@ -1,16 +1,26 @@
-let codecount=document.getElementsByTagName('pre').length;
-for(let i=1 ; i<=codecount ; i++){
-    document.getElementById('code'+i).insertAdjacentHTML('beforebegin','<div id="button"><button id="button'+i+'" onclick="copy('+i+')">copy</button></div>');
+document.addEventListener('DOMContentLoaded', function() {
+document.getElementsByTagName('h1')[0].insertAdjacentHTML('afterend','<div id="tag"></div>')
+var tag_target=document.getElementsByTagName('h2')
+for (i=0;i<tag_target.length;i++){
+    tag_target[i].insertAdjacentHTML('beforebegin','<hr id="'+tag_target[i].innerText+'">')
+    document.getElementById('tag').insertAdjacentHTML('beforeend','<a href="#'+tag_target[i].innerText+'">'+(i+1)+'.'+tag_target[i].innerText+'</a>\n')
 }
-var img = document.getElementsByTagName('img');
-for(let i=0 ; i<img.length ; i++){
-    img[i].setAttribute("alt","画像の読み込みに失敗しました。");
+document.getElementById('tag').insertAdjacentHTML('beforebegin','<h2>目次<h2>')
+let code_target=document.getElementsByTagName('pre')
+for(let i=0;i<=code_target.length;i++){
+    code_target[i].setAttribute('id','code'+i)
+    document.getElementById('code'+i).insertAdjacentHTML('beforebegin','<div id="button"><button id="button'+i+'" onclick="copy('+i+')">copy</button></div>')
+}
+var img = document.getElementsByTagName('img')
+for(let i=0;i<img.length;i++){
+    img[i].setAttribute('alt','画像の読み込みに失敗しました。')
 }
 function copy(target_id){
-    var target = document.getElementById("code"+target_id);
-    navigator.clipboard.writeText(target.textContent);
-    document.getElementById("button"+target_id).textContent = 'copied!';
+    var target = document.getElementById('code'+target_id)
+    navigator.clipboard.writeText(target.textContent)
+    document.getElementById('button'+target_id).textContent = 'copied!'
     setTimeout(function() {
-        document.getElementById("button"+target_id).textContent = 'copy';
-    }, 1000);
+        document.getElementById('button'+target_id).textContent = 'copy'
+    }, 1000)
 }
+})
