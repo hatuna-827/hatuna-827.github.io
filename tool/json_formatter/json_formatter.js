ce8bb1a44d010a698ba912314b726f63d55909df
hatuna-827 <hatuna827yadaka@gmail.com>
2025-12-02 13:31:01 +0900

    add download copy button

--- tool/json_formatter/json_formatter.js
+++ tool/json_formatter/json_formatter.js
@@ -19,55 +19,81 @@ const format_structure = {
 	}
 }
 const gui_structure = {
-	is_array: {
+	top_type: {
 		type: "select", display_name: "トップレベルの構造", option: {
-			object: { display_name: "オブジェクト" },
-			array: { display_name: "リスト" }
+			object: {
+				display_name: "オブジェクト", children: {
+					sort: { type: "boolean", display_name: "自動ソート" },
+					entry: { type: "reference", name: "entry" }
+				}
+			},
+			array: {
+				display_name: "リスト", children: {
+					type: { type: "reference", name: "select_type" }
+				}
+			}
 		}
 	},
-	sort: { type: "select", display_name: "自動ソート", option: { no: { display_name: "しない" }, yes: { display_name: "する" } } },
-	entry: { type: "reference", name: "entry" }
 }
 const entry_structure = {
 	entry: {
 		type: "list", display_name: "エントリー", children: {
 			type: "object", children: {
 				key: { type: "string", display_name: "プロパティ名", placeholder: "(必須)" },
-				type: {
-					type: "select", display_name: "型", option: {
-						string: { children: { default: { type: "toggle", display_name: "デフォルトの値", children: { type: "string" } } } },
-						number: { children: { default: { type: "toggle", display_name: "デフォルトの値", children: { type: "number" } } } },
-						object: { children: { sort: { type: "select", display_name: "自動ソート", option: { no: { display_name: "しない" }, yes: { display_name: "する" } } }, entry: { type: "reference", name: "entry" } } },
-						array: { children: { sort: { type: "select", display_name: "自動ソート", option: { no: { display_name: "しない" }, yes: { display_name: "する" } } }, entry: { type: "reference", name: "entry" } } },
-						bool: { children: { default: { type: "toggle", display_name: "デフォルトの値", children: { type: "boolean" } } } },
-						null: { children: {} }
-					}
-				}
+				type: { type: "reference", name: "select_type" }
 			}
 		}
 	}
 }
-
-// const struct = [
-// 	{ type: "string", display_name: "", min: 0, max: 0, default: "", pattern: "", placeholder: "" },
-// 	{ type: "number", display_name: "", min: 0, max: 0, step: 0, default: 0, pattern: "", placeholder: "" },
-// 	{ type: "boolean", display_name: "", default: false },
-// 	{ type: "toggle", display_name: "", children: struct, default: false },
-// 	{ type: "list", display_name: "", children: struct, min: 0, max: 0 },
-// 	{ type: "object", display_name: "", children: { name: struct } },
-// 	{ type: "select", display_name: "", option: { option_name: { display_name: "", children: { name: struct } } } },
-// 	{ type: "select", display_name: "", option: [option_name] },
-// 	{ type: "reference", name: struct_name }
-// ]
+const select_type_structure = {
+	type: {
+		type: "select", display_name: "型", option: {
+			string: { children: { default: { type: "toggle", display_name: "デフォルトの値", children: { type: "string" } } } },
+			number: { children: { default: { type: "toggle", display_name: "デフォルトの値", children: { type: "number" } } } },
+			object: { children: { sort: { type: "boolean", display_name: "自動ソート" }, entry: { type: "reference", name: "entry" } } },
+			array: { children: { sort: { type: "boolean", display_name: "自動ソート" }, entry: { type: "reference", name: "entry" } } },
+			bool: { children: { default: { type: "toggle", display_name: "デフォルトの値", children: { type: "boolean" } } } },
+			null: { children: {} }
+		}
+	}
+}
 /* - init -------------------------------------------------------------------------------------- */
 set_input_file()
 set_format_input_file()
 structure.def_struct("format", format_structure)
 structure.def_struct("gui", gui_structure)
 structure.def_struct("entry", entry_structure)
+structure.def_struct("select_type", select_type_structure)
 structure.set("input-format-structure", "format", change_format)
-structure.set("input-format-gui-structure", "gui")
+structure.set("input-format-gui-structure", "gui", change_format_gui)
 change_format()
+change_format_gui()
+
+document.querySelectorAll(".textarea-copy-download").forEach((element) => {
+	const button_box = document.createElement('div')
+	button_box.className = "copy-download-box"
+	const download_button = document.createElement('div')
+	download_button.className = "download-button button"
+	download_button.textContent = "ダウンロード"
+	const copy_button = document.createElement('div')
+	copy_button.className = "copy-button button"
+	copy_button.textContent = "コピー"
+	download_button.addEventListener('click', function () {
+		const download = document.createElement('a')
+		download.setAttribute('download', "download.json")
+		const content = `data:application/json;base64,${btoa(element.value || element.textContent)}`
+		download.setAttribute('href', content)
+		download.click()
+		clicked(this)
+	})
+	copy_button.addEventListener('click', function () {
+		navigator.clipboard.writeText(element.value || element.textContent)
+		clicked(this)
+	})
+	button_box.appendChild(download_button)
+	button_box.appendChild(copy_button)
+	element.insertAdjacentElement('beforebegin', button_box)
+})
 /* - add eventListener ------------------------------------------------------------------------- */
 document.getElementById("input-json-area").addEventListener('input', set_input_file_name)
 document.getElementById("execute").addEventListener('click', execute)
@@ -119,10 +145,10 @@ function set_format_input_file() {
 }
 function change_format() {
 	const format = structure.get("input-format-structure").format
-	const gui_format = document.getElementById("input-format-gui-structure")
 	const text_format = document.getElementById("input-format-text")
-	gui_format.style.display = "none"
+	const gui_format = document.getElementById("input-format-gui")
 	text_format.style.display = "none"
+	gui_format.style.display = "none"
 	if (format.structure === "no") {
 	} else if (format.structure === "text") {
 		text_format.style.display = "block"
@@ -131,42 +157,81 @@ function change_format() {
 
 	}
 }
+function change_format_gui() {
+	document.getElementById("input-format-gui-area").textContent = JSON.stringify(structure.get("input-format-gui-structure").gui, null, 2)
+}
 // output
 function execute() {
-	const input_validity = document.getElementById("input-validity")
 	const output_field = document.getElementById("output-field")
 	reset_output()
 	const json = parse_json(document.getElementById("input-json-area").value)
 	if (json.status === "error") {
-		const log_field = document.getElementById("log-field")
-		input_validity.textContent = "Error"
-		log_field.textContent = json.value
+		set_result("error")
+		log("入力されたJSONが無効です。")
+		log(json.value)
 		return
 	}
+
 	const format_data = structure.get("input-format-structure").format
-	const indent_type = format_data.indent_type
-	let result
-	if (indent_type === "space") {
-		result = JSON.stringify(json.value, null, format_data.indent_count)
-	} else if (indent_type === "tab") {
-		result = JSON.stringify(json.value, null, "\t".repeat(format_data.indent_count))
-	} else if (indent_type === "other") {
-		result = JSON.stringify(json.value, null, format_data.indent_text)
-	} else if (indent_type === "no_indent") {
-		result = JSON.stringify(json.value)
+	let result = {}
+
+	const structure_type = format_data.structure
+	if (structure_type === "no") {
+		result = json.value
+	} else if (structure_type === "text") {
+	} else if (structure_type === "gui") {
+		const format_gui_data = structure.get("input-format-gui-structure").gui
+		console.log(format_gui_data)
 	}
-	input_validity.textContent = "OK"
-	output_field.innerText = result
+
+	const indent_type = format_data.indent_type
+	if (indent_type === "space") {
+		result = JSON.stringify(result, null, format_data.indent_count)
+	} else if (indent_type === "tab") {
+		result = JSON.stringify(result, null, "\t".repeat(format_data.indent_count))
+	} else if (indent_type === "other") {
+		result = JSON.stringify(result, null, format_data.indent_text)
+	} else if (indent_type === "no_indent") {
+		result = JSON.stringify(result)
+	}
+	set_result("sucess")
+	output_field.textContent = result
 }
 function parse_json(json) {
 	try {
+		if (!/\S*\{/.test(json)) {
+			return { status: "error", value: "JSONは'{'または'['で始まる必要があります。" }
+		}
 		return { status: "success", value: JSON.parse(json) }
 	} catch (err) {
 		return { status: "error", value: err }
 	}
 }
+function log(content) {
+	const log_field = document.getElementById("log-field")
+	log_field.textContent += content
+	log_field.textContent += "\n"
+}
+function set_result(result) {
+	const result_element = document.getElementById("result")
+	if (result === "error") {
+		result_element.innerHTML = "Error"
+		result_element.style.color = "var(--red-color)"
+	} else if (result === "sucess") {
+		result_element.innerHTML = "OK"
+		result_element.style.color = "var(--main-dark-color)"
+	}
+}
 function reset_output() {
-	document.getElementById("input-validity").textContent = ""
+	document.getElementById("result").textContent = ""
 	document.getElementById("output-field").textContent = ""
+	document.getElementById("log-field").textContent = ""
+}
+// download copy
+function clicked(target) {
+	target.classList.add("clicked")
+	setTimeout(() => {
+		target.classList.remove("clicked")
+	}, 0)
 }
 /* --------------------------------------------------------------------------------------------- */
