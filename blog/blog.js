95508ff6729541649e9217037f7993159ae4e325
hatuna-827 <hatuna827yadaka@gmail.com>
2025-12-10 10:24:06 +0900

    update blog.css

--- blog/blog.js
+++ blog/blog.js
@@ -1,64 +1,93 @@
 document.addEventListener('DOMContentLoaded', function () {
+	const content = document.createElement('div')
+	content.id = "main-content"
+	const contents_list = Array.from(document.getElementById("main").children)
+	contents_list.forEach((element) => {
+		content.appendChild(element)
+	})
+	document.getElementById("main").appendChild(content)
+
 	document.querySelectorAll('img').forEach((img) => {
 		img.setAttribute('alt', '画像の読み込みに失敗しました。')
 		const imgbox = document.createElement('div')
-		imgbox.className = "imgbox"
-		imgbox.appendChild(img.cloneNode(false))
+		imgbox.className = "img-box"
 		img.insertAdjacentElement('afterend', imgbox)
-		img.remove()
+		imgbox.appendChild(img)
 	})
 	img_fit()
+
 	document.querySelectorAll('#main a').forEach((link) => {
 		if (!link.getAttribute('target')) {
-			if (!link.getAttribute('href').startsWith("#"))
+			if (!link.getAttribute('href').startsWith("#")) {
 				link.setAttribute('target', '_blank')
+			}
 		}
 	})
-	// 目次の追加
-	const tag = document.createElement("div")
-	tag.id = "tag"
-	document.querySelector("h1").insertAdjacentElement("afterend", tag)
-	const tag_target = document.querySelectorAll('h2')
-	tag_target.forEach((target, i) => {
-		const tag_text = target.innerText.replace(/"/g, "'")
-		const tag_hr = document.createElement("hr")
-		const tag_link = document.createElement("a")
-		const tag_br = document.createElement("br")
-		tag_hr.id = tag_text
-		tag_link.textContent = (i + 1) + '.' + target.innerText
-		tag_link.setAttribute('href', "#" + tag_text)
-		target.insertAdjacentElement('beforebegin', tag_hr)
-		tag.insertAdjacentElement('beforeend', tag_link)
-		tag.insertAdjacentElement('beforeend', tag_br)
+
+	const tag_container = document.createElement('div')
+	tag_container.id = "tag-container"
+	const tag_wrapper = document.createElement('div')
+	tag_wrapper.id = "tag-wrapper"
+	const tag_content = document.createElement('div')
+	tag_content.id = "tag-content"
+	const tag_list = document.createElement('ol')
+	tag_list.id = "tag-list"
+	const tag_title = document.createElement('h2')
+	tag_title.id = "tag-title"
+	tag_title.textContent = "目次"
+	document.querySelectorAll('h2').forEach((subtitle, i) => {
+		const esc_text = subtitle.innerText.replace(/"/g, "'")
+		const link_target = document.createElement('hr')
+		const list = document.createElement('li')
+		const link = document.createElement('a')
+		link_target.id = esc_text
+		link.textContent = subtitle.innerText
+		link.setAttribute('href', "#" + esc_text)
+		subtitle.insertAdjacentElement('beforebegin', link_target)
+		list.appendChild(link)
+		tag_list.appendChild(list)
 	})
-	tag.insertAdjacentHTML('beforebegin', '<h2>目次<h2>')
-	// コピーボタンの追加
-	const code_target = document.querySelectorAll('pre')
-	code_target.forEach((target, i) => {
+	tag_content.appendChild(tag_list)
+	tag_wrapper.appendChild(tag_title)
+	tag_wrapper.appendChild(tag_content)
+	tag_container.appendChild(tag_wrapper)
+	document.querySelector("h1").insertAdjacentElement("afterend", tag_container)
+
+	document.querySelectorAll('pre').forEach((target, i) => {
 		target.id = "code" + i
 		const pre_box = document.createElement('div')
 		pre_box.className = "pre-box"
-		pre_box.appendChild(target.cloneNode(true))
-		pre_box.insertAdjacentHTML('afterbegin', '<div class="sticky-box"><button id="button' + i + '" class="copy-btn" onclick="copy(' + i + ')">copy</button></div>')
+		const sticky_box = document.createElement('div')
+		sticky_box.className = "sticky-box"
+		const copy_button = document.createElement('div')
+		copy_button.className = "copy-btn"
+		const copy_button_icon = document.createElement('div')
+		copy_button_icon.className = "copy-btn-icon"
+		fetch("https://hatuna-827.github.io/icons/xml/copy.xml").then(r => r.text()).then(svg => { copy_button_icon.innerHTML = svg })
+		const copy_button_text = document.createElement('div')
+		copy_button_text.className = "copy-btn-text"
+		copy_button_text.textContent = "コードをコピー"
+		copy_button.addEventListener('click', function () {
+			navigator.clipboard.writeText(target.textContent)
+			copy_button_text.textContent = "コピーしました"
+			setTimeout(() => {
+				copy_button_text.textContent = "コードをコピー"
+			}, 1000)
+		})
 		target.insertAdjacentElement('afterend', pre_box)
-		target.remove()
+		copy_button.appendChild(copy_button_text)
+		copy_button.appendChild(copy_button_icon)
+		sticky_box.appendChild(copy_button)
+		pre_box.appendChild(target)
+		pre_box.appendChild(sticky_box)
 	})
 })
 window.addEventListener('resize', img_fit)
-function copy(target_id) {
-	const target = document.getElementById('code' + target_id)
-	navigator.clipboard.writeText(target.textContent)
-	document.getElementById('button' + target_id).textContent = 'copied!'
-	setTimeout(function () {
-		document.getElementById('button' + target_id).textContent = 'copy'
-	}, 1000)
-}
 function img_fit() {
 	document.querySelectorAll('img').forEach((img) => {
 		img.parentElement.style.height = "50vh"
 		img.parentElement.style.width = "calc(100% - 60px)"
 		img.style.height = "auto"
-		img.style.width = "auto"
 		img.style.width = img.parentElement.offsetWidth + "px"
 		if (img.parentElement.offsetHeight < img.offsetHeight) {
 			img.style.width = "auto"
