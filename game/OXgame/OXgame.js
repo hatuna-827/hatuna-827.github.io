58715cce667ae47289379cbf5214deaf51f25dc8
hatuna-827 <hatuna827yadaka@gmail.com>
2026-05-20 17:51:39 +0900

    update OXgame

--- game/OXgame/OXgame.js
+++ game/OXgame/OXgame.js
@@ -1,61 +1,61 @@
-let turn
-let data
-let game
+/* - const ------------------------------------------------------------------------------------- */
+let data = []
+let tiles = []
+let turn = 0
+/* - init -------------------------------------------------------------------------------------- */
+const board = document.getElementById('board')
+for (let i = 0; i < 9; ++i) {
+	const tile = document.createElement('div')
+	tile.addEventListener('click', function () {
+		if ((turn !== 0) & (data[i] === 0)) {
+			data[i] = turn
+			tiles[i].classList.add(turn === 1 ? 'O' : 'X')
+			check()
+			turn *= -1
+		}
+	})
+	board.appendChild(tile)
+	tiles.push(tile)
+}
 reset()
+/* - add eventListener ------------------------------------------------------------------------- */
+document.getElementById('reset').addEventListener('click', reset)
+document.getElementById('pop').addEventListener('click', hidepop)
+/* - function ---------------------------------------------------------------------------------- */
 function reset() {
-	for (i = 1; i <= 9; i++) {
-		let target = document.getElementById(i)
-		target.textContent = '・'
-		target.classList.remove('button-display')
+	tiles.forEach(tile => {
+		tile.className = 'tile'
+	})
+	data = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+	turn = 1
+}
+function check() {
+	if (
+		[
+			data[0] + data[1] + data[2],
+			data[3] + data[4] + data[5],
+			data[6] + data[7] + data[8],
+			data[0] + data[3] + data[6],
+			data[1] + data[4] + data[7],
+			data[2] + data[5] + data[8],
+			data[0] + data[4] + data[8],
+			data[2] + data[4] + data[6],
+		].includes(turn * 3)
+	) {
+		end(turn)
+	} else if (!data.includes(0)) {
+		end(0)
 	}
-	data = ['', '', '', '', '', '', '', '', '']
-	turn = '〇'
-	game = 0
 }
-function draw() {
-	document.getElementById('ifdraw').textContent = 'draw!'
-	document.getElementById('winner').textContent = '-'
-}
-function win() {
-	game = 1
-	document.getElementById('winner').textContent = turn
-	document.getElementById('pop').style.display = 'flex'
-	document.getElementById('pop').classList.add('pop-display')
+
+function end(winner) {
+	document.getElementById('winner').className = winner === 0 ? 'draw' : winner === 1 ? 'O' : 'X'
+	document.getElementById('ifdraw').textContent = winner === 0 ? 'draw!' : 'winner!'
+	document.getElementById('pop').className = 'display'
+	turn = 0
 }
 function hidepop() {
-	document.getElementById('pop').style.display = 'none'
-	document.getElementById('ifdraw').textContent = 'winner!'
+	document.getElementById('pop').className = ''
 	reset()
 }
-function push(num) {
-	if ((document.getElementById(num).textContent == '・') & (game == 0)) {
-		document.getElementById(num).textContent = turn
-		document.getElementById(num).classList.add('button-display')
-		data.splice(num - 1, 1, turn)
-		if ((data[0] == data[1]) & (data[1] == data[2]) & (data[2] != '')) {
-			win()
-		} else if ((data[3] == data[4]) & (data[4] == data[5]) & (data[5] != '')) {
-			win()
-		} else if ((data[6] == data[7]) & (data[7] == data[8]) & (data[8] != '')) {
-			win()
-		} else if ((data[0] == data[3]) & (data[3] == data[6]) & (data[6] != '')) {
-			win()
-		} else if ((data[1] == data[4]) & (data[4] == data[7]) & (data[7] != '')) {
-			win()
-		} else if ((data[2] == data[5]) & (data[5] == data[8]) & (data[8] != '')) {
-			win()
-		} else if ((data[0] == data[4]) & (data[4] == data[8]) & (data[8] != '')) {
-			win()
-		} else if ((data[2] == data[4]) & (data[4] == data[6]) & (data[6] != '')) {
-			win()
-		} else if (!data.includes('')) {
-			win()
-			draw()
-		}
-		if (turn == '〇') {
-			turn = '×'
-		} else {
-			turn = '〇'
-		}
-	}
-}
+/* --------------------------------------------------------------------------------------------- */
